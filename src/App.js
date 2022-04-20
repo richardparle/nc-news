41478e22501210f92fb73e08879b6143c4ee941d
richardparle <richardparle@gmail.com>
2022-04-20 10:25:09 +0100

    task 4 complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Articles from "./components/Articles";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Articles />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Articles.jsx
@@ -0,0 +1,27 @@
+import { getArticles } from "../utils/api";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Articles = () => {
+   const [articles, setArticles] = useState([])
+
+   useEffect(() => {
+      getArticles()
+      .then((articlesFromApi => {
+         setArticles(articlesFromApi)
+      }))
+   }, [])
+      console.log(articles, '<< 2')
+   return <div>
+      <ul>
+         {articles.map(article => {
+            return <li key={article.article_id}>
+               <h2>{article.title}</h2>
+               <p>{article.author}</p>
+               </li>
+         })}
+      </ul>
+   </div>
+}
+
+export default Articles;
