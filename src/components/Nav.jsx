3296c46b8225e2e9cd8bdefe0d7d6431f199e5fe
richardparle <richardparle@gmail.com>
2022-04-21 11:20:49 +0100

    add link to home page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,8 @@ import {Link} from 'react-router-dom'
 const Nav = () => {
    return (
       <nav>
-         <Link to='/api/topics'>Topics</Link>
+         <Link to='/' className='navTab'>Home</Link>
+         <Link to='/api/topics' className='navTab'>Topics</Link>
       </nav>
    )
 }
